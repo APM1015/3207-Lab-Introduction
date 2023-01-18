86ab418081d730ce08241136ab93a53fa21d8c08
APM1015 <122992723+APM1015@users.noreply.github.com>
2023-01-18 09:47:30 -0500

    Create random.c

---
+++ random.c
@@ -0,0 +1,14 @@
+int main()
+{
+  String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	
+  int a;
+		
+/* seed the randomizer */
+	srand( (unsigned)time(NULL) );
+
+	for(a=0;a<27;a++)
+		putf('%c',a);
+
+	return(0);
+}
