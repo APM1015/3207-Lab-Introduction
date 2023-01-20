ff32332cc689f21c04371a3b5f111bd3025e78e4
APM1015 <122992723+APM1015@users.noreply.github.com>
2023-01-20 15:24:55 -0500

    Update random.c

--- random.c
+++ random.c
@@ -4,15 +4,27 @@
 
 int main()
 {
-  String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	
+	int c = rand() % 26;
+	char ch = (char)(char+65);
+	putf('%c',ch);
+	return 0;
+	
+	/*
+  String chars[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	
   int a;
+  string letter = "";
 		
 /* seed the randomizer */
+	/*
 	srand( (unsigned)time(NULL) );
 
 	for(a=0;a<27;a++)
 		putf('%c',a);
 
 	return(0);
+	*/
+	
+	
 }
